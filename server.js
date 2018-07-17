3a7b57342b946efbc0326e35fc082c7e46a08fb3
hoang <hoang_lp_97@yahoo.com>
2018-07-17 15:52:27 +0200

    first test

---
+++ server.js
@@ -0,0 +1,22 @@
+const io = require('socket.io')();
+
+const desDate = new Date("2018-08-04T15:00:00").getTime();
+const orgDate = new Date("2018-07-06T02:00:00").getTime();
+
+const totalTime = desDate - orgDate;
+
+console.log(orgDate);
+
+io.on('connection', (client) => {
+    // here you can start emitting events to the client 
+    client.on('subscribeToTimer', (interval) => {
+        console.log('client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+          client.emit('timer', {percent: (new Date().getTime() - orgDate) / totalTime * 100, left: desDate - new Date().getTime()});
+        }, interval);
+    });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { subscribeToTimer } from './api';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({ 
+      timestamp 
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+        This is the timer value: {Math.round(this.state.timestamp.percent * 10000) / 10000 + '%'}
+        </p>
+        <p className="App-intro">
+          days left: {Math.round(this.state.timestamp.left/86400000 * 10) / 10}
+        </p>
+        <div className="container">
+          <div className="block" style={{height: this.state.timestamp.percent + '%'}}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
