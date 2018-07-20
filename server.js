a0c5034bb8e027be7489414c1c1dc5339e8e59f6
khangduoc102 <hoang_lp_97@yahoo.com>
2018-07-21 00:29:46 +0200

    update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const io = require('socket.io')();
 
 const desDate = new Date("2018-08-04T15:00:00").getTime();
@@ -20,3 +21,29 @@ io.on('connection', (client) => {
 const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
+*/
+var app = require('express')();
+var http = require('http').Server(app);
+var cors = require('cors');
+var io = require('socket.io')(http);
+
+const desDate = new Date("2018-08-04T15:00:00").getTime();
+const orgDate = new Date("2018-07-06T02:00:00").getTime();
+
+const totalTime = desDate - orgDate;
+
+app.use(cors());
+
+io.on('connection', function(client){
+    console.log('a user connected');
+    client.on('subscribeToTimer', (interval) => {
+        console.log('client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+          client.emit('timer', {percent: (new Date().getTime() - orgDate) / totalTime * 100, left: desDate - new Date().getTime()});
+        }, interval);
+    });
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
