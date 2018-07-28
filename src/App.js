95a674e85c34d06eb7843c7abb14972c89e4d4f0
khangduoc102 <hoang_lp_97@yahoo.com>
2018-07-28 12:51:11 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { subscribeToTimer } from './api';
-
 class App extends Component {
   constructor(props) {
     super(props);
-    subscribeToTimer((err, timestamp) => this.setState({ 
-      timestamp 
-    }));
+    
+  }
+
+  resolveAfter1Seconds = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('resolved');
+      }, 5000);
+    });
   }
 
   state = {
+    desDate: new Date("2018-08-04T15:00:00").getTime(),
+    orgDate: new Date("2018-07-06T02:00:00").getTime(),
     timestamp: 'no timestamp yet'
   };
+
+  componentDidMount= () => {
+    const totalTime = this.state.desDate - this.state.orgDate;
+    console.log(totalTime);
+    window.setInterval(() => {
+      this.setState(() => ({timestamp: {percent: (new Date().getTime() - this.state.orgDate) / totalTime * 100, left: this.state.desDate - new Date().getTime()}}));
+    }, 1000)
+  }
 
   render() {
     return (
